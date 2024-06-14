09581a67a6c09f14693669218b6e6568be947ea8
smirnov <smirnov@alfasatcom.ru>
2024-06-14 11:12:31 +0300

    работа с header

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from "./components/Header";
 
-function App() {
-  let [count, setCount] = useState(0)
-  const aloha='aloha is greeting'
-  const handleClick=()=>{
-    setCount(++count)
-    console.log(count)
-  }
-
-  return (
-    <>
+function App(){
+  return(
     <div>
-      <h1>{aloha}</h1>
-      {/* <button onClick={()=>alert('aloha')}>Click me</button> */}
-      <button onClick={handleClick}>Click me</button>
-      <p>count is {count}</p>
+      <Header />
+      <main>
+        Content
+      </main>
     </div>
-    </>
   )
-}
 
-export default App
+}
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header(){
+    return(
+        <header>
+            Menu
+            <button>Click me</button>
+        </header>
+    )
+}
+export default Header;
