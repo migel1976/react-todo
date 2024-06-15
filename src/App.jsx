89e5927f40e794995afc177d082d876518e8d593
smirnov <smirnov@alfasatcom.ru>
2024-06-15 11:50:36 +0300

    мапирование массива статей

--- src/App.jsx
+++ src/App.jsx
@@ -27,8 +27,9 @@ function App(){
       <main>
         <section>
           <ul>
-            <TodoList {...todoList[0]} />
-            <TodoList {...todoList[1]} />
+            {todoList.map(item=><TodoList {...item} key={item.text}/>)}
+            {/* <TodoList {...todoList[0]} /> */}
+            {/* <TodoList {...todoList[1]} /> */}
             {/* <TodoList title={todoList[0].text} text={todoList[0].title}/> */}
             {/* <TodoList title={todoList[1].text} text={todoList[1].title}/> */}
           </ul>
